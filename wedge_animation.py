10b1d261ecf5ae515b497e60147545611693a803
superPershing <727342027@qq.com>
2017-12-12 18:52:28 +0800

    for save

--- wedge_animation.py
+++ wedge_animation.py
@@ -9,6 +9,8 @@ from matplotlib import animation
 import matplotlib.patches
 
 robot_nums = 4
+update_interval = 100
+speed_init = 0.5
 
 class Robot: #TODO
     def __init__(self, x_init, y_init, init_angle, speed_init):
@@ -19,17 +21,16 @@ class Robot: #TODO
         self.dx = 1 / math.sqrt(1 + self.k ** 2)
         self.dy = math.sqrt(1 - self.dx**2)
         self.speed = speed_init
-    
     # def random_walk(self):
         
 
-    def follow(self):
+    def follow(self, robots):
         pass
     
     def talk(self):
         pass
 
-    def update_direction(self): #TODO stuck in corner and border.
+    def update_direction(self):
         if (self.x + self.dx) < 10 and (self.x + self.dx) > -10 and (self.y + self.dy) < 10 and (self.y + self.dy) > -10:
             self.x += self.dx    
             self.y += self.dy
@@ -50,24 +51,22 @@ class Robot: #TODO
             self.dx = 1 / math.sqrt(1 + self.k ** 2)
             self.dy = - math.sqrt(1 - self.dx ** 2)
 
-# robot0 =  Robot(random.randint(-10, 10), random.randint(-10, 10), 0, 1)
-robots = [Robot(random.randint(-10, 10), random.randint(-10, 10), 0, 1) for i in range(4)]
+
+def judge_overlaping():
+    pass
+
+robots = [Robot(random.randint(-10, 10), random.randint(-10, 10), 0, speed_init) for i in range(4)]
 
 
 # First set up the figure, the axis, and the plot element we want to animate
 fig = plt.figure()
 ax = plt.axes(xlim=(-12, 12), ylim=(-12, 12))
 # plt.grid(True)
-# patch_one = matplotlib.patches.Circle((0, 0), 1)
-# patch_one = matplotlib.patches.Wedge((robot0.x,robot0.y), 1, robot0.angle - 25, robot0.angle + 25)
 patchs = [matplotlib.patches.Wedge((robot_i.x,robot_i.y), 1, robot_i.angle - 25, robot_i.angle + 25) for robot_i in robots]
 patchs_tail = [matplotlib.patches.Wedge((robot_i.x,robot_i.y), 1, robot_i.angle + 90, robot_i.angle + 270) for robot_i in robots]
 
 # initialization function: plot the background of each frame
 def init():
-    # patch_one.radius = 1
-    # ax.add_patch(patch_one)
-    # return patch_one,
     for i in range(4):
         patchs[i].radius = 1
         patchs_tail[i].radius = 1
@@ -78,14 +77,6 @@ def init():
 
 # animation function.  This is called sequentially
 def animate(frame):
-    # robot0.update_direction()
-    # # robot0.random_walk()
-    # patch_one.set_center((robot0.x, robot0.y))
-    # patch_one.set_theta1(robot0.angle - 25)
-    # patch_one.set_theta2(robot0.angle + 25)
-    # patch_one._recompute_path()
-    # print(robot0.angle)
-    # return patch_one,
     for i in range(4):
         robots[i].update_direction()
         # robot0.random_walk()
@@ -107,6 +98,6 @@ def animate(frame):
 
 # call the animator.  blit=True means only re-draw the parts that have changed.
 anim = animation.FuncAnimation(fig, animate, init_func=init,
-                               frames=200, interval=100, blit=True)
+                               frames=200, interval=update_interval, blit=True)
 
 plt.show()
